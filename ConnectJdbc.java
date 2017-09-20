f6356f943b8bd15664cd2fa1393b9976c038c081
Thetida <thetidatsantaki@gmail.com>
2017-09-20 13:47:00 +0300

    Add files via upload

---
+++ ConnectJdbc.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * Created by thetida on 9/20/17.
+ */
+public class ConnectJdbc {
+    public void Connection(){
+
+        
+
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * Created by thetida on 9/20/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ConnectJdbc example= new ConnectJdbc();
+        example.Connection();
+    }
+}
